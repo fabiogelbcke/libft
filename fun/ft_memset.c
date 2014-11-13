8c77755c2746990feaf4d0e7978504ec42702496
Fabio SCHUBERT-GELBCKE <fschuber@e3r9p9.42.fr>
2014-11-13 13:31:34 +0100

    memset norminetted

--- fun/ft_memset.c
+++ fun/ft_memset.c
@@ -6,22 +6,23 @@
 /*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 13:00:04 by fschuber          #+#    #+#             */
-/*   Updated: 2014/11/13 13:29:08 by fschuber         ###   ########.fr       */
+/*   Updated: 2014/11/13 13:31:02 by fschuber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdlib.h>
 
-void *ft_memset(void *b, int c, size_t len)
+void		*ft_memset(void *b, int c, size_t len)
 {
-	int i;
-	char *str;
+	int		i;
+	char	*str;
 
 	str = b;
 	i = 0;
 	while (i < len)
 	{
-		str[i] =  c;
+		str[i] = c;
 		i++;
 	}
-	return b;
+	return (b);
 }
