1ef4f8decd5c7d7fda8afaadc952f414097c0920
Fabio SCHUBERT-GELBCKE <fschuber@e1r8p6.42.fr>
2014-11-21 02:19:16 +0100

    make and .h

---
+++ Makefile
@@ -0,0 +1,76 @@
+#******************************************************************************#
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2014/11/21 02:01:24 by fschuber          #+#    #+#              #
+#    Updated: 2014/11/21 02:07:02 by fschuber         ###   ########.fr        #
+#                                                                              #
+#******************************************************************************#
+
+
+NAME = libft.a
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+SRC =	ft_atoi.c \
+		ft_bzero.c \
+		ft_isalnum.c \
+		ft_isalpha.c \
+		ft_isascii.c \
+		ft_isdigit.c \
+		ft_isprint.c \
+		ft_memalloc.c \
+		ft_memccpy.c \
+		ft_memset.c \
+		ft_putchar.c \
+		ft_putendl.c \
+		ft_putstr.c \
+		ft_strcat.c \
+		ft_strchr.c \
+		ft_strclr.c \
+		ft_strcmp.c \
+		ft_strcpy.c \
+		ft_strdup.c \
+		ft_strequ.c \
+		ft_striter.c \
+		ft_striteri.c \
+		ft_strjoin.c \
+		ft_strlcat.c \
+		ft_strmap.c \
+		ft_strncat.c \
+		ft_strncmp.c \
+		ft_strncpy.c \
+		ft_strnequ.c \
+		ft_strnstr.c \
+		ft_strrchr.c \
+		ft_strsplit.c \
+		ft_strstr.c \
+		ft_strsub.c \
+		ft_strtrim.c \
+		ft_tolower.c \
+		ft_toupper.c
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME):
+	@$(CC) $(CFLAGS) -c $? $(SRC)
+	@ar rc $(NAME) $? $(OBJ)
+	@ranlib $(NAME)
+	@echo "\033[32mBuilt library.\033[0m"
+
+clean:
+	@/bin/rm -f $(OBJ)
+	@echo "\033[32mCleaned up object files.\033[0m"
+
+fclean: clean
+	@/bin/rm $(NAME)
+	@echo "\033[32mCleaned up compiled files.\033[0m"
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- libft.h
+++ libft.h
@@ -3,12 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: plavaux <plavaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/19 12:19:11 by fschuber          #+#    #+#             */
-/*   Updated: 2014/11/19 12:20:26 by fschuber         ###   ########.fr       */
+/*   Created: 2014/11/03 11:40:29 by plavaux           #+#    #+#             */
+/*   Updated: 2014/11/21 02:19:00 by fschuber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memalloc(size_t size);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar(char c);
+void	ft_putendl(char const *s);
+void	ft_putstr(char const *s);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
+void	ft_strclr(char *s);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strdup(const char *s1);
+int		ft_strequ(char const *s1, char const *s2);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *s1, const char *s2, size_t n);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *s1, const char *s2);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	**ft_strsplit(char const *s, char c);
+char	*ft_strtrim(char const *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+#endif
