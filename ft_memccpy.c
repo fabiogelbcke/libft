52b9eac691b4146c124f395fbb8250b547128221
Fabio SCHUBERT-GELBCKE <fschuber@e3r4p13.42.fr>
2014-11-26 16:08:42 +0100

    sorta stuck

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/21 06:28:35 by fschuber          #+#    #+#             */
-/*   Updated: 2014/11/26 15:07:24 by fschuber         ###   ########.fr       */
+/*   Updated: 2014/11/26 15:42:45 by fschuber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 	while (i < (int)n)
 	{
 		*ptr1 = *ptr2;
-		ptr1++;
-		ptr2++;
 		if (*ptr2 == (unsigned char)c)
 		{
 			ptr2++;
 			return (void*)ptr2;
 		}
+		ptr1++;
+		ptr2++;
 		i++;
 	}
 	return NULL;
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,26 +6,45 @@
 /*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/21 06:35:15 by fschuber          #+#    #+#             */
-/*   Updated: 2014/11/23 04:57:04 by fschuber         ###   ########.fr       */
+/*   Updated: 2014/11/26 16:08:05 by fschuber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-	char *ptr;
+	unsigned char *ptr;
+	unsigned char *ptrdst;
+	unsigned char *temp;
 	int i;
-	void *ret;
 
-	ret = dst;
 	i = 0;
-	ptr = (char*)src;
+	if (!dst || !src)
+		return NULL;
+	temp = (void*)malloc(len);
+	ptr = (unsigned char*)src;
 	while (i < (int)len)
 	{
-		*(char*)dst = *(ptr++);
-		dst = (char*)dst + 1;
+		temp[i] = ptr[i];
 		i++;
 	}
-	return ret;
+	i = 0;
+	ptr = (unsigned char*)src;
+	ptrdst = (unsigned char*)dst;
+	while (i < (int)len)
+	{
+		ptrdst[i] = ptr[i];
+		i++;
+	}
+	return dst;
+}
+
+int main ()
+{
+	char str[] = "memmove can be very useful......";
+	memmove (str+20,str+15,11);
+	puts (str);
+	return 0;
 }
