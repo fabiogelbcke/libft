b07674c6ca078ead161e7657928a40ee88fc45c3
Fabio SCHUBERT-GELBCKE <fschuber@e2r4p17.42.fr>
2014-11-27 23:34:37 +0100

    still shit to do tomorrow

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,17 +6,17 @@
 /*   By: fschuber <fschuber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/19 09:22:09 by fschuber          #+#    #+#             */
-/*   Updated: 2014/11/21 02:37:56 by fschuber         ###   ########.fr       */
+/*   Updated: 2014/11/27 21:38:04 by fschuber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
 #include <stdlib.h>
 
-int getlen(char const *s)
+static int	getlen(char const *s)
 {
-	int len;
-	int i;
+	int		len;
+	int		i;
 
 	len = 0;
 	i = 0;
@@ -25,30 +25,37 @@ int getlen(char const *s)
 	while(s[i++])
 		len++;
 	if (len == 0)
-		return 0;
+		return (0);
+	i--;
 	i--;
 	while(s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
 	{
 		i--;
 		len--;
 	}
-	return len;
+	return (len);
 }
 
-char *ft_strtrim(char const *s)
+char		*ft_strtrim(char const *s)
 {
-	char *ret;
-	int size;
-	int startindex;
-	int i;
+	char	*ret;
+	int		size;
+	int		startindex;
+	int		i;
 
 	i = 0;
 	size = getlen(s);
-	ret = malloc(sizeof(char) * (size + 1));
+	ret = (char*)malloc(sizeof(char) * (size + 1));
+	if (!ret || !s)
+		return (NULL);
 	while(s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
 		i++;
 	startindex = i;
-	while(i - startindex >= size)
+	while(i - startindex < size)
+	{
 		ret[i - startindex] = s[i];
-	return ret;
+		i++;
+	}
+	ret[i - startindex] = '\0';
+	return (ret);
 }
